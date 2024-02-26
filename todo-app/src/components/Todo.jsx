f5fff2d8f0a9856440a77bcdcba2d9aaadb25932
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-02-26 12:14:46 -0500

    Submitting Todo Items
    - array to hold todos
    - prevent default auto refresh on submit
    - utilize spread operator on list to update todos

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,12 +1,18 @@
-import useState from "react"
+import {useState} from "react"
 
 export default function Todo() {
     const [todo, setTodo] = useState("")
-    return <div>
-        <form>
-            <h1>{todo}</h1>
-            <input value={todo} type="text" onChange={()=> setTodo(e.target.value)}/>
+    const [todos, setTodos] = useState([])
+    function handleSubmit(e) {
+        e.preventDefault()
+        setTodos([...todos, todo])
+        setTodo("")
+    }
+    return (<div>
+        <form onSubmit={handleSubmit}>
+            <h1>{todos}</h1>
+            <input value={todo} type="text" onChange={(e)=> setTodo(e.target.value)}/>
             <button type="submit">Add</button>
         </form>
-    </div>
+    </div>)
 }
