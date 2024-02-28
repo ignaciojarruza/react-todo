d31a1845ca6d51a695d00603f4f3e68f2ed5cb07
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-02-28 11:51:54 -0500

    Separating JSX Into Components
    - putting [state, stateFunction] into parent <Todo/> component
    - passing necessary variables into children <TodoList/> and <Form/>

---
+++ todo-app/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+export default function Form({todos, setTodos}) {
+    const [todo, setTodo] = useState("")
+    
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setTodos([...todos, todo])
+        setTodo("")
+    }
+
+    return <form onSubmit={handleSubmit}>
+    <input value={todo} type="text" onChange={(e)=> setTodo(e.target.value)}/>
+    <button type="submit">Add</button>
+</form>
+}
--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,19 +1,11 @@
+import TodoList from "./TodoList"
+import Form from "./Form"
 import {useState} from "react"
-import TodoItem from "./TodoItem"
 
 export default function Todo() {
-    const [todo, setTodo] = useState("")
     const [todos, setTodos] = useState([])
-    function handleSubmit(e) {
-        e.preventDefault()
-        setTodos([...todos, todo])
-        setTodo("")
-    }
     return (<div>
-        <form onSubmit={handleSubmit}>
-            <h1>{todos.map(item=><TodoItem item={item} key={item}/>)}</h1>
-            <input value={todo} type="text" onChange={(e)=> setTodo(e.target.value)}/>
-            <button type="submit">Add</button>
-        </form>
+        <Form todos={todos} setTodos={setTodos}/>
+        <TodoList todos={todos}/>
     </div>)
 }
